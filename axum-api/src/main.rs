098810f9238a4153e0edbad55ca9840edcf334bc
Petre Razvan <petre.ispir2002@protonmail.com>
2026-07-13 22:15:33 +0300

    first start

---
+++ axum-api/src/main.rs
@@ -0,0 +1,12 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+// curl http://localhost:3000
