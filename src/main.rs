ae7589166edb9f807a3e2e9a330f358e4b8d0bec
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-18 00:52:13 +0300

    added anyhow, argon2, dotenvy and sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use dotenvy::dotenv;
+
 use mini_axum_event_planner::{event_planner_listener, event_planner_router};
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     axum::serve(event_planner_listener().await, event_planner_router())
         .await
         .unwrap();
