ae7912d3101d810794ef5a6f5971318697b64f32
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-19 23:51:47 +0300

    added CRUD for the event planner using sqlx

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,10 +1,18 @@
+use askama::Template;
+use askama_web::WebTemplate;
 use axum::{http::StatusCode, response::IntoResponse};
 
-use crate::template_structs::{Homepage, NotFound};
+#[derive(Template, WebTemplate)]
+#[template(path = "homepage.html")]
+pub struct Homepage {}
 
 pub async fn homepage() -> Homepage {
     Homepage {}
 }
+
+#[derive(Template, WebTemplate)]
+#[template(path = "not_found.html")]
+pub struct NotFound {}
 
 pub async fn not_found() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, NotFound {})
