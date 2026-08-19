4bd7190f9230b1675bbdebfa5319c53c2e5b3ec0
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-19 23:47:48 +0300

    added axum error handling for anyhow

---
+++ src/error.rs
@@ -0,0 +1,25 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        eprintln!("{:?}", self.0);
+
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Something went wrong on our end. Try again in a moment.",
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
