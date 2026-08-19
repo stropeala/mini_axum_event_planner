18eafec8d1721455ee23650bd967a5e85ed26a0b
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-19 23:52:20 +0300

    added different router for events

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,48 @@
 mod authentication;
 mod db;
+mod error;
 mod handlers;
-mod template_structs;
 
 use axum::{Router, routing::get};
+use sqlx::SqlitePool;
 use tokio::net::TcpListener;
 
-use handlers::home::{homepage, not_found};
+use db::{create_table, sqlite_pool};
+use handlers::{
+    events::{
+        get_event_delete, get_event_planner_add, get_event_planner_description,
+        get_event_planner_index, post_event_delete, post_event_planner_add,
+    },
+    home::{homepage, not_found},
+};
 
-pub fn event_planner_router() -> Router {
-    Router::new().route("/", get(homepage)).fallback(not_found)
+const EVENTS: &str = "EVENT_PLANNER_SCHEMA";
+const USER: &str = "USER_SCHEMA";
+
+pub async fn event_planner_pool() -> anyhow::Result<SqlitePool> {
+    let pool = sqlite_pool().await?;
+    create_table(&pool, EVENTS).await?;
+    create_table(&pool, USER).await?;
+    Ok(pool)
+}
+
+pub fn events_router() -> Router<SqlitePool> {
+    Router::new()
+        .route("/", get(get_event_planner_index))
+        .route(
+            "/add",
+            get(get_event_planner_add).post(post_event_planner_add),
+        )
+        .route("/delete", get(get_event_delete).post(post_event_delete))
+        .route("/{id}", get(get_event_planner_description))
+}
+
+pub fn event_planner_router(pool: SqlitePool) -> Router {
+    Router::new()
+        .route("/", get(homepage))
+        .nest("/events", events_router())
+        .fallback(not_found)
+        .with_state(pool)
 }
 
 pub async fn event_planner_listener() -> TcpListener {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use dotenvy::dotenv;
 
-use mini_axum_event_planner::{event_planner_listener, event_planner_router};
+use mini_axum_event_planner::{event_planner_listener, event_planner_pool, event_planner_router};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
-    axum::serve(event_planner_listener().await, event_planner_router())
-        .await
-        .unwrap();
+    let pool = event_planner_pool().await?;
+    axum::serve(event_planner_listener().await, event_planner_router(pool)).await?;
+    Ok(())
 }
